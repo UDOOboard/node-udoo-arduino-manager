17882099c0856494bac16915e31c924d609e1d5e
harlem88 <harlem88.ant@gmail.com>
2017-05-22 17:09:11 +0200

    added firmwares

--- test.js
+++ test.js
@@ -3,6 +3,21 @@ var Arduino = require('./lib/');
 var arduino = new Arduino('ttyACM1');
 
 
-arduino.setPinMode(7, 1);
-arduino.digitalWrite(7, 0);
+// arduino.setPinMode(7, 1);
+// arduino.digitalWrite(7, 0);
 // arduino.digitalWrite(7, 1);
+
+arduino.isInstalled(function (err, installed) {
+    if(err && !installed){
+        console.log('not installed try to upload default sketch');
+        arduino.uploadSketch('neo', function (err, data) {
+            if(err) console.log(err);
+            else console.log(data);
+        });
+    }else {
+        arduino.version(function (err, data) {
+            if(err) console.log(err);
+            else console.log('version installed -> ' + data);
+        })
+    }
+});
