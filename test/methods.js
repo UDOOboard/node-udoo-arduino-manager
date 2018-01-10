96df9ef78fc19965a468547f70570c1da1e61102
Francesco Montefoschi <francesco.monte@gmail.com>
2018-01-10 11:35:43 +0100

    Moved tests in a separate directory

---
+++ test/methods.js
@@ -0,0 +1,88 @@
+var Arduino = require('../lib/udoo-arduino-manager');
+var arduino = new Arduino('ttyACM0');
+var async = require('async');
+var LOG = require('../lib/udoo-arduino-manager/logger');
+
+async.series([
+    (done) => {
+        LOG.W('Blinking LED 13');
+        arduino.digitalWrite(13, 1);
+        setTimeout(done, 500);
+    }, 
+    (done) => {
+        arduino.digitalWrite(13, 0);
+        setTimeout(done, 500);
+    }, 
+    (done) => {
+        arduino.digitalWrite(13, "high");
+        setTimeout(done, 500); 
+    },
+    (done) => {
+        arduino.digitalWrite(13, "low");
+        setTimeout(done, 1000);
+    },
+
+    (done) => {
+        LOG.W('Blinking LED 13 (timed)');
+        arduino.timedDigitalWrite(13, 1, 250);
+        setTimeout(done, 600);
+    }, 
+    (done) => {
+        arduino.timedDigitalWrite(13, 1, 250);
+        setTimeout(done, 600);
+    }, 
+
+    (done) => {
+        LOG.W('Digital read');
+        arduino.digitalRead(10);
+        setTimeout(done, 300);
+    }, 
+
+    (done) => {
+        LOG.W('Analog read');
+        arduino.analogRead(0);
+        setTimeout(done, 300);
+    }, 
+
+    (done) => {
+        LOG.W('Analog read (repeated)');
+        arduino.repeatedAnalogRead(0, 6, 100);
+        setTimeout(done, 1000);
+    }, 
+
+    (done) => {
+        LOG.W('Analog write');
+        arduino.analogWrite(9, 128);
+        setTimeout(done, 100);
+    }, 
+
+    (done) => {
+        LOG.W('Interrupt (attach)');
+        arduino.attachInterrupt(3, 2, function() {
+            console.log(arguments);
+        });
+        setTimeout(done, 100);
+    }, 
+    
+    (done) => {
+        arduino.attachInterrupt(5, 2, function() {
+            console.log(arguments);
+        });
+        setTimeout(done, 100);
+    }, 
+
+    (done) => {
+        LOG.W('Interrupt (detach)');
+        arduino.detachInterrupt(5);
+        setTimeout(done, 100);
+    }, 
+
+    (done) => {
+        LOG.W('I2C sensors scanner');
+        arduino.scanI2Csensors();
+        setTimeout(done, 3000);
+    }, 
+
+], function() {
+    arduino.closePort();
+});
---
+++ test/test.js
@@ -0,0 +1,42 @@
+var Arduino = require('../lib/udoo-arduino-manager');
+var arduino = new Arduino('ttyACM0');
+var async = require('async');
+
+arduino.isInstalled(function (err, installed) {
+    if (err && !installed) {
+        console.log('Belzedoo is not installed, please wait...');
+        
+	arduino.uploadSketch('x86', function (err, data) {
+             if(err) console.log(err);
+             else console.log(data);
+         });
+    } else {
+        arduino.version(function (err, data) {
+            if(err) console.log(err);
+            else console.log('version installed -> ' + data);
+
+            async.series([
+                (done) => {
+                    arduino.digitalWrite(13, 1);
+                    setTimeout(done, 500);
+                }, 
+                (done) => {
+                    arduino.digitalWrite(13, 0);
+                    setTimeout(done, 500);
+                }, 
+                (done) => {
+                    arduino.digitalWrite(13, 1);
+                    setTimeout(done, 500); 
+                },
+                (done) => {
+                    arduino.digitalWrite(13, 0);
+                    setTimeout(done, 500);
+                }
+
+            ], function() {
+                arduino.closePort();
+            });
+        })
+    }
+});
+
